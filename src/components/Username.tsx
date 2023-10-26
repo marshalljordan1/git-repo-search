938b1b0b87e8210ea96409b4765e71c6d4317882
marshalljordan1 <jordan_marshall@live.co.uk>
2023-10-27 01:54:59 +0200

    Increase username font size

--- src/components/Username.tsx
+++ src/components/Username.tsx
@@ -6,10 +6,8 @@ const Username: React.FC = () => {
   const user = useSelector((state: RootState) => state.github.user);
 
   return (
-    <div className="flex flex-col ms-5">
-      <p className="font-bold sm:text-2xl text-3xl mt-5 sm:mt-5">
-        {user?.name}
-      </p>
+    <div className="flex flex-col ms-5 sm:ms-0">
+      <p className="font-bold text-3xl mt-5">{user?.name}</p>
       <p className="font-normal sm:text-xl text-2xl text-light-gray font-light">
         {user?.login}
       </p>
